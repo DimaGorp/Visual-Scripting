dec3b62f6f78b085dba9ca35f4fe0143e696d9fb
LodinBohdan <tba1312@gmail.com>
2025-02-26 23:01:17 +0200

    Create base design

--- src/main/java/org/example/visualscripting/HelloController.java
+++ src/main/java/org/example/visualscripting/HelloController.java
@@ -2,13 +2,20 @@ package org.example.visualscripting;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Button;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
+    @FXML private AnchorPane workspacePane;
+
+    @FXML private Button btnLoadFile;
+
+    @FXML private Button btnSaveFile;
+
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
     }
 }
