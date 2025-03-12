3450315eeb584df7474843f83b3968af09432789
Tymur Bielykh <tymurbielykh@gmail.com>
2025-03-12 18:14:50 +0200

    basic UI added, for Bogdan <3

--- src/main/java/org/example/visualscripting/MainController.java
+++ src/main/java/org/example/visualscripting/MainController.java
@@ -3,12 +3,17 @@ package org.example.visualscripting;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainController {
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    protected void onInputBlockClick() {
+        welcomeText.setText("Input Block Clicked!");
     }
 }
