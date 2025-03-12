1ec6fe07d1ebb5d6d988110ac27c3fb14993b221
Tymur Bielykh <tymurbielykh@gmail.com>
2025-03-12 17:32:33 +0200

    json controller added, diagram writing added

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,6 +9,7 @@ module org.example.visualscripting {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
+    requires com.fasterxml.jackson.databind;
 
     opens org.example.visualscripting to javafx.fxml;
     exports org.example.visualscripting;
